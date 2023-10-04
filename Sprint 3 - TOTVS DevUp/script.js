2cc76f15da3a4bde2d77582f43afd80160b7f377
Alexsander248 <alexsander.assis@outlook.com>
2023-10-04 10:51:15 -0300

    Update Sprint 3

--- Sprint 3 - TOTVS DevUp/script.js
+++ Sprint 3 - TOTVS DevUp/script.js
@@ -1,14 +1,14 @@
 function coletaDados() {
   const dataNasc = coletaDataNasc();
-  coletaCPF();
-  coletaNomeCliente();
-  coletaEmail();
-  coletaNumeroContato();
-  coletaEndereco();
-  coletaCidade();
-  coletaProfissao();
-  coletaSalario();
-  coletaSenha();
+  const cpf = coletaCPF();
+  const nomeCliente = coletaNomeCliente();
+  const email = coletaEmail();
+  const numerosContato = coletaNumeroContato();
+  const endereco = coletaEndereco();
+  const cidade = coletaCidade();
+  const profissao = coletaProfissao();
+  const salario = coletaSalario();
+  const senha = coletaSenha();
 }
 
 coletaDados();
@@ -21,14 +21,20 @@ function coletaDataNasc() {
 
 function coletaCPF() {
   const cpf = prompt("Qual o seu CPF? (xxx.xxx.xxx.-xx)");
+
+  return cpf;
 }
 
 function coletaNomeCliente() {
   const nomeCliente = prompt("Digite o seu nome completo:");
+
+  return nomeCliente;
 }
 
 function coletaEmail() {
   const email = prompt("Agora nos diga qual é o seu melhor email:");
+
+  return email;
 }
 
 function coletaNumeroContato() {
@@ -36,28 +42,43 @@ function coletaNumeroContato() {
   const telefone = prompt(
     "Digite um número de telefone para contato. Caso não possua clique em OK para prosseguir."
   );
+
+  return {
+    celular: celular,
+    telefone: telefone,
+  };
 }
 
 function coletaEndereco() {
   const endereco = prompt(
     "Qual o seu endereço? (R. das Flores, 195, Bairro Tristeza"
   );
+
+  return endereco;
 }
 
 function coletaCidade() {
   const cidade = prompt("Qual cidade e estado você mora? (Porto Alegre - RS)");
+
+  return cidade;
 }
 
 function coletaProfissao() {
   const profissao = prompt("Conta para nós, qual é a sua profissão?");
+
+  return profissao;
 }
 
 function coletaSalario() {
   const salario = prompt("E qual a sua renda mensal aproximada?");
+
+  return salario;
 }
 
 function coletaSenha() {
   const senha = prompt(
     "Agora digite uma senha de 4 números (não pode repetir os números nem ser uma sequência)"
   );
+
+  return senha;
 }
